ba4d518636d9bdf1b53be4f7c88aad735bfa954f
Zahra Elair <zahraelair17@gmail.com>
2022-09-19 18:04:49 +0100

    final touches

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import ME from '../../assets/pic.png'
-import HeaderSocials from './HeaderSocials'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/pic.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return (
-    <header>
-      <div className="container header__container">
-        <h5> Hello I'm </h5>
-        <h1>Elair Zahra</h1>
-        <h5 className="text-light">Front-end Developer</h5>
-        <CTA/>
-        <HeaderSocials/>
-        <div className="me">
-          <img src={ME} alt="me" />
-        </div>
-        <a href="#contact" className="scroll__down">Scroll Down</a>
-        
-      </div>
-    </header>
-  )
-}
+    return (
+        <header>
+            <div className="container header__container">
+                <h5> Hello I'm </h5>
+                <h1>Elair Zahra</h1>
+                <h5 className="text-light">Web Developer</h5>
+                <CTA />
+                <HeaderSocials />
+                <div className="me">
+                    <img src={ME} alt="me" />
+                </div>
+                <a href="#contact" className="scroll__down">
+                    Scroll Down
+                </a>
+            </div>
+        </header>
+    );
+};
 
-export default Header
+export default Header;
